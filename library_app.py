cc6825c5e2f565a9b3773ec7e6fd42866dca2639
Kanya <74917282+kanyavasudev@users.noreply.github.com>
2026-02-22 15:56:12 +0530

    Update library_app.py

--- library_app.py
+++ library_app.py
@@ -5,12 +5,12 @@ import pandas as pd
 # ── DATABASE CONNECTION ──────────────────────────────────
 def get_connection():
     return mysql.connector.connect(
-        host=st.secrets["DB_HOST"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASSWORD"],
-        database=st.secrets["DB_NAME"],
-        port=int(st.secrets["DB_PORT"])
+        host="localhost",
+        user="root",
+        password="Kanya#005",
+        database="library_management",
     )
+
 # ── LOAD CSS ─────────────────────────────────────────────
 def load_css():
     with open("style.css") as f:
